d2153d98459fc89430aa192e1112388f666841ea
Samuel Tan <samueltango@gmail.com>
2014-10-27 22:20:10 -0400

    fix the location label refresh issue

--- Challenge5/src/TestGraphics.java
+++ Challenge5/src/TestGraphics.java
@@ -21,7 +21,7 @@ class DrawLine extends JFrame {
     public DrawLine() {
         p = getContentPane();
         int window_X, window_Y;
-        
+
         window_X = (Constants.SCREEN_WIDTH - Constants.WINDOW_WIDTH) / 2;
         window_Y = (Constants.SCREEN_HEIGHT - Constants.WINDOW_HEIGHT) / 2;
         setBounds(window_X, window_Y, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
@@ -35,36 +35,56 @@ class DrawLine extends JFrame {
     public void paintComponents(Graphics gg) {
 //        gg.drawLine(10, 100, 200, 400);
         final Graphics g = gg;
+        final int clear_block_width = 200;       // Repainting area that needs to be cleared to display new location value
+        final int clear_block_height = 12;      // Repainting area that needs to be cleared to display new location value
         start = new Point(0, 80);
         end = new Point(0, 80);
-        g.setColor(Color.red);
+        g.setColor(Color.blue);
         Runnable run = new Runnable() {
             Point temp = null;
             int x = 0;
             int y = 200;
+            String strLocation;
+            boolean outOfScope = false;
+
             public void run() {
                 int d = 1;
                 while(true) {
                     try {
-//                        temp = new Point(x, 80+(int)(40*Math.sin(Math.PI*(x-80)/30)));
-                        temp = new Point(x, y);
-                        g.drawLine(start.x, start.y, end.x, end.y);
-                        g.clearRect(192, Constants.WINDOW_HEIGHT - 20, 50, 12);
-                        g.drawString("Sensor Location: " + x + ", " + y, 100, Constants.WINDOW_HEIGHT - 10);
-                        start = end;
-                        end = temp;
-                        Thread.sleep(200);
+                        System.out.println(x + ", " + y);
+                        if(y>10000){
+                            x = 150;
+                            y = 230;
+                        }
+                        if(x>800 || x<0 || y>600 || y<0) {
+                            g.setColor(Color.RED);
+                            strLocation = "Sensor Location: Out of detection scope!";
+                            if(!outOfScope)
+                                g.clearRect(Constants.WINDOW_WIDTH / 2 - 8, Constants.WINDOW_HEIGHT - 20, clear_block_width, clear_block_height);
+
+                            g.drawString(strLocation, Constants.WINDOW_WIDTH / 2 - 100, Constants.WINDOW_HEIGHT - 10);
+                            outOfScope = true;
+
+                        }else {
+                            g.setColor(Color.BLUE);
+                            // Show the sensor at the location
+//                            temp = new Point(x, 80+(int)(40*Math.sin(Math.PI*(x-80)/30)));
+                            temp = new Point(x, y);
+                            g.drawLine(start.x, start.y, end.x, end.y);
+
+                            // Update location label
+                            strLocation = "Sensor Location: " + x + ", " + y;
+                            g.clearRect(Constants.WINDOW_WIDTH / 2 - 8, Constants.WINDOW_HEIGHT - 20, clear_block_width, clear_block_height);
+                            g.drawString(strLocation, Constants.WINDOW_WIDTH / 2 - 100, Constants.WINDOW_HEIGHT - 10);
+
+                            // Move to next location
+                            start = end;
+                            end = temp;
+                            outOfScope = false;
+                            Thread.sleep(10);
+                        }
                     } catch (Exception e) {
                         e.printStackTrace();
-                    }
-                    if(x>800 || x<0) {
-                        d = -d;
-                        Color c = g.getColor();
-                        if(c == Color.RED) {
-                            g.setColor(Color.BLUE);
-                        } else {
-                            g.setColor(Color.RED);
-                        }
                     }
                     x += d;
                     y += d;

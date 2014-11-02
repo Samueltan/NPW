817ef2eb1a1914da5a506f7a8a1111e3cc36b58c
Samuel Tan <samueltango@gmail.com>
2014-11-02 15:03:27 -0500

    fix the repaint issue

--- Challenge5/src/TestGraphics.java
+++ Challenge5/src/TestGraphics.java
@@ -58,49 +58,10 @@ class DrawLocation extends JFrame {
 
                 while(true) {
                     try {
-                        g.setColor(Color.GRAY);
-                        // Canvas area is defined to be a 800 * 600 area
-//                        g.drawRect(Constants.CANVAS_MARGIN_WIDTH, Constants.CANVAS_MARGIN_HEIGHT,
-//                            Constants.CANVAS_WIDTH, Constants.CANVAS_HEIGHT);       // (20, 50, 800, 600)
-
-                        // Define the x, y axis
-                        // x axis: (x0, yn) ~ (xn, yn)
-                        // y axis: (x0, y0) ~ (x0, yn)
-                        int x0 = Constants.CANVAS_MARGIN_WIDTH;
-                        int y0 = Constants.CANVAS_MARGIN_HEIGHT;
-                        int xn = x0 + Constants.CANVAS_WIDTH;
-                        int yn = y0 + Constants.CANVAS_HEIGHT;
-                        g.drawLine(x0, yn, xn, yn);     // draw x axis
-                        g.drawLine(x0, y0, x0, yn);     // draw y axis
-                        int tickInt = Constants.CANVAS_WIDTH / 10;
-                        for (int xt = x0 + tickInt; xt < xn; xt += tickInt) {
-                            g.drawLine(xt, yn + 5, xt, yn - 5);
-                            int min = (xt - x0) / 1;
-                            g.drawString(Integer.toString(min), xt - (min < 10 ? 3 : 7) , yn + 20);
-                        }
-
-                        tickInt = Constants.CANVAS_HEIGHT / 10;
-                        for (int yt = y0 + tickInt; yt < yn; yt += tickInt) {
-                            g.drawLine(x0 - 5, yt, x0 + 5, yt);
-                            int min = (yt - y0) / 1;
-                            g.drawString(Integer.toString(min), x0 - 32 , yt + 5);
-                        }
-
-                        // Show the fixed sensors' locations
-                        Color blue = Color.BLUE;
-                        drawCenteredCircle(g, blue, x0, yn, 10);
-                        g.drawString("Sensor A", x0 - 30 , yn + 18);
-                        drawCenteredCircle(g, blue, xn, yn, 10);
-                        g.drawString("Sensor B", xn - 30 , yn + 18);
-                        drawCenteredCircle(g, blue, x0, y0, 10);
-                        g.drawString("Sensor C", x0 - 30 , y0 - 10);
-                        drawCenteredCircle(g, blue, xn, y0, 10);
-                        g.drawString("Sensor D", xn - 30 , y0 - 10);
-
 
                         // Draw the sensor track in real-time mode
-//                        y = Constants.CANVAS_MARGIN_HEIGHT + (int)(40*Math.sin(Math.PI*(x-Constants.CANVAS_MARGIN_WIDTH)/30));
-                        y = Constants.CANVAS_MARGIN_HEIGHT + 3*(x-Constants.CANVAS_MARGIN_WIDTH)/4;
+                        y = Constants.CANVAS_MARGIN_HEIGHT + (int)(40*Math.sin(Math.PI*(x-Constants.CANVAS_MARGIN_WIDTH)/30));
+//                        y = Constants.CANVAS_MARGIN_HEIGHT + 3*(x-Constants.CANVAS_MARGIN_WIDTH)/4;
 //                        System.out.println(x + ", " + y);
                         if(!isInScope(x,y)) {
 //                            System.out.println("Not in scope!");
@@ -115,15 +76,15 @@ class DrawLocation extends JFrame {
 //                            break;
                         }else {
 //                            System.out.println("In scope!");
-                            g.setColor(Color.BLUE);
                             // Show the sensor at the location
 //                            temp = new Point(x, 60+(int)(40*Math.sin(Math.PI*(x-80)/30)));
                             temp = new Point(x, y);
 //                            g.drawLine(start.x, start.y, end.x, end.y);
-                            g.clearRect(x-5, y-5, 10, 10);
+                            cleanCanvas(g);
                             drawCenteredCircle(g, Color.RED, x, y, 8);
 
                             // Update location label
+                            g.setColor(Color.BLUE);
                             strLocation = "Sensor Location: " + x + ", " + y;
                             g.clearRect(windowWidth / 2 - 8, windowHeight - 20, clear_block_width, clear_block_height);
                             g.drawString(strLocation, windowWidth / 2 - 100, windowHeight - 10);
@@ -133,6 +94,33 @@ class DrawLocation extends JFrame {
                             end = temp;
                             outOfScope = false;
                         }
+
+                        // Draw the x, y axis
+                        g.setColor(Color.GRAY);
+                        drawXYAxis(g);
+
+                        // Show the fixed sensors' locations
+                        /**
+                         *          C               D
+                         *
+                         *
+                         *          A               B
+                         */
+                        Point pA, pB, pC, pD;
+                        int x1 = Constants.CANVAS_MARGIN_WIDTH;
+                        int y1 = Constants.CANVAS_MARGIN_HEIGHT + Constants.CANVAS_HEIGHT;
+                        int x2 = Constants.CANVAS_MARGIN_WIDTH + Constants.CANVAS_WIDTH;
+                        int y2 = Constants.CANVAS_MARGIN_HEIGHT + Constants.CANVAS_HEIGHT;
+                        int x3 = Constants.CANVAS_MARGIN_WIDTH;
+                        int y3 = Constants.CANVAS_MARGIN_HEIGHT;
+                        int x4 = x2;
+                        int y4 = y3;
+                        pA = new Point(x1, y1);
+                        pB = new Point(x2, y2);
+                        pC = new Point(x3, y3);
+                        pD = new Point(x4, y4);
+                        drawFixedSensors(g, pA, pB, pC, pD);
+
                         Thread.sleep(10);
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -145,11 +133,61 @@ class DrawLocation extends JFrame {
         new Thread(run).start();
     }
 
+    public void drawXYAxis(Graphics g){
+
+        // x axis: (x0, yn) ~ (xn, yn)
+        // y axis: (x0, y0) ~ (x0, yn)
+        int x0 = Constants.CANVAS_MARGIN_WIDTH;
+        int y0 = Constants.CANVAS_MARGIN_HEIGHT;
+        int xn = x0 + Constants.CANVAS_WIDTH;
+        int yn = y0 + Constants.CANVAS_HEIGHT;
+        g.drawLine(x0, yn, xn, yn);     // draw x axis
+        g.drawLine(x0, y0, x0, yn);     // draw y axis
+        int tickInt = Constants.CANVAS_WIDTH / 10;
+        for (int xt = x0 + tickInt; xt < xn; xt += tickInt) {
+            g.drawLine(xt, yn + 5, xt, yn - 5);
+            int min = (xt - x0) / 1;
+            g.drawString(Integer.toString(min), xt - (min < 10 ? 3 : 7) , yn + 20);
+        }
+
+        tickInt = Constants.CANVAS_HEIGHT / 10;
+        for (int yt = y0 + tickInt; yt < yn; yt += tickInt) {
+            g.drawLine(x0 - 5, yt, x0 + 5, yt);
+            int min = (yt - y0) / 1;
+            g.drawString(Integer.toString(min), x0 - 32 , yt + 5);
+        }
+    }
+
+    public void drawFixedSensors(Graphics g, Point pointA, Point pointB, Point pointC, Point pointD) {
+        Color color = Color.BLUE;
+        // Sensor A
+        drawCenteredCircle(g, color, pointA.x, pointA.y, 10);
+        g.drawString("Sensor A", pointA.x - 30, pointA.y + 18);
+
+        // Sensor B
+        drawCenteredCircle(g, color, pointB.x, pointB.y, 10);
+        g.drawString("Sensor B", pointB.x - 30, pointB.y + 18);
+
+        // Sensor C
+        drawCenteredCircle(g, color, pointC.x, pointC.y, 10);
+        g.drawString("Sensor C", pointC.x - 30, pointC.y - 10);
+
+        // Sensor D
+        drawCenteredCircle(g, color, pointD.x, pointD.y, 10);
+        g.drawString("Sensor D", pointD.x - 30, pointD.y - 10);
+    }
+
     public void drawCenteredCircle(Graphics gg, Color color, int x, int y, int r) {
         gg.setColor(color);
         x = x-(r/2);
         y = y-(r/2);
         gg.fillOval(x,y,r,r);
+    }
+
+    public void cleanCanvas(Graphics gg){
+        int cleanX = Constants.CANVAS_MARGIN_WIDTH + 1;
+        int cleanY = Constants.CANVAS_MARGIN_HEIGHT - 5;
+        gg.clearRect(cleanX, cleanY, Constants.CANVAS_WIDTH, Constants.CANVAS_HEIGHT);
     }
 
     public boolean isInScope(int x, int y){

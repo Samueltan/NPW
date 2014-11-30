a406221994fd2e0f9136fd646f4c7279d5f020f2
Samuel Tan <samueltango@gmail.com>
2014-11-29 21:06:10 -0500

    dummy savetoDB() introduced with no error

--- Challenge7_8/servlet/src/HelloWorldServlet.java
+++ Challenge7_8/servlet/src/HelloWorldServlet.java
@@ -1,10 +1,17 @@
+import dao.generated.tables.Location;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Date;
-import java.util.Enumeration;
+//import java.util.Enumeration;
 
 /**
  * Created by Samuel on 2014/11/26.
@@ -38,6 +45,8 @@ public class HelloWorldServlet extends HttpServlet {
         bw.newLine();
         bw.flush();
 
+        System.out.println("Dummy db operation **********2 ...");
+
         PrintWriter pw = resp.getWriter();
         pw.println("<html><head><title>Hi</title></head><body>Hello world!!!!!  See hello.log.</body></html>");
         pw.flush();
@@ -45,5 +54,34 @@ public class HelloWorldServlet extends HttpServlet {
         bw.close();
 
         System.out.println("Exiting doPost()...");
+    }
+
+    public void saveToDB(String address, String timestamp, int dr, int cx){
+        System.out.println("Entering saveToDB()...");
+
+//        java.sql.Connection conn = null;
+//        try {
+//            // For Sqlite
+//            Class.forName("org.sqlite.JDBC").newInstance();
+//            conn = DriverManager.getConnection("jdbc:sqlite:challenges.db");
+//
+//            DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+//
+//            create.insertInto(Location.LOCATION, Location.LOCATION.ADDR, Location.LOCATION.TIME,
+//                    Location.LOCATION.DISTANCER, Location.LOCATION.CENTERX)
+//                    .values(address, timestamp, dr, cx).execute();
+//
+//        } catch (Exception e) {
+//            // For the sake of this tutorial, let's keep exception handling simple
+//            e.printStackTrace();
+//        } finally {
+//            if (conn != null) {
+//                try {
+//                    conn.close();
+//                } catch (SQLException ignore) {
+//                }
+//            }
+//        }
+        System.out.println("Exiting saveToDB()...");
     }
 }
